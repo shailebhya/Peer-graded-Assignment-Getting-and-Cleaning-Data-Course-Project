682f0cf1971703a18f0554ff5f6fce63aa855662
shailebhya <55810482+shailebhya@users.noreply.github.com>
2022-04-28 23:34:05 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -54,5 +54,3 @@ library(plyr);
 Data2<-aggregate(. ~subject + activity, Data, mean)
 Data2<-Data2[order(Data2$subject,Data2$activity),]
 write.table(Data2, file = "tidydata.txt",row.name=FALSE)
-library(knitr)
-knit2html("codebook.Rmd");
